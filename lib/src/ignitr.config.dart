ab2c5a1e32f44957965f3536a6559e3d94968440
iamspydey <shoabkhan33@gmail.com>
2026-06-04 00:42:42 +0530

    prepared for release

--- lib/src/ignitr.config.dart
+++ lib/src/ignitr.config.dart
@@ -1,8 +1,8 @@
 class Config {
   static final String version = "v0.2.0";
-  static final bool inDevMode = true;
+  static final bool inDevMode = false;
   static final String devTemplateUrl = "https://github.com/ignitr-dev/ignitr/archive/refs/heads/main.zip";
-  static final String releasedTemplateUrl = "https://api.github.com/repos/ignitr-dev/ignitr/releases/tags/$version.zip";
+  static final String releasedTemplateUrl = "https://github.com/ignitr-dev/ignitr/archive/refs/tags/$version.zip";
 
   static String get templateUrl => inDevMode ? devTemplateUrl : releasedTemplateUrl;
 }
